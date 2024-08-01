341b26ad057e65d35244c372bcf82596489b94f4
KDRK47 <kdrk47@gmail.com>
2024-08-01 16:02:40 +0300

    Update index.js

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -21,11 +21,6 @@ export const Home = () => {
           <div className="text order-2 order-lg-1 h-100 d-lg-flex justify-content-center">
             <div className="align-self-center ">
               <div className="intro mx-auto">
-        <div key={i} className="po_item">
-                <img src={logoimage_url} alt="" />
-                <div className="content">
-                </div>
-              </div>
                 <h2 className="mb-1x">{introdata.title}</h2>
                 <h1 className="fluidz-48 mb-1x">
                 IT Manager
