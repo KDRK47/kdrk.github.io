db51184f0271c69fa4792c09d2921559be1108ae
KDRK47 <kdrk47@gmail.com>
2024-08-02 13:48:50 +0300

    Update routes.js

--- src/app/routes.js
+++ src/app/routes.js
@@ -8,19 +8,6 @@ import { About } from "../pages/about";
 import { Socialicons } from "../components/socialicons";
 import { CSSTransition, TransitionGroup } from "react-transition-group";
 
-async function getAPI("https://kadirdurak.com/") {
-    const response = await fetch("https://kadirdurak.com/")
-    if(!response.ok) {
-        return getAPI("https://kadirdurak.com/")
-    }
-    var data = await response.json()
-    var g = data.genres
-
-    let user_genre = document.getElementById("input").value
-    for (let i = 0; i < g.length; i++) {
-        console.log(g[i].name)
-    }
-}
 
 const AnimatedRoutes = withRouter(({ location }) => (
   <TransitionGroup>
