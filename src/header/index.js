183a8813ed5c5b56017891c3d3c79f51deedb9cf
KDRK47 <kdrk47@gmail.com>
2024-08-02 14:06:39 +0300

    Update index.js

--- src/header/index.js
+++ src/header/index.js
@@ -64,7 +64,7 @@ const Headermain = () => {
       <header className="fixed-top site__header">
         <div className="d-flex align-items-center justify-content-between">
     <Link  className="navbar-brand nav_ac" to="/">
-               <img src="{ logoimage_url }" /> { logotext }
+               <img src="https://kadirdurak.com/assets/images/kdrklogo.png" /> { logotext }
           </Link>
           <div className="d-flex align-items-center">
           <Themetoggle />
