acc6570f40bb8e6fb5162653951da7d842c62eac
KDRK47 <kdrk47@gmail.com>
2024-08-02 09:07:43 +0300

    Update index.js

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -19,8 +19,6 @@ export const Home = () => {
             style={{ backgroundImage: `url(${introdata.your_img_url})` }}
           ></div>
           <div className="text order-2 order-lg-1 h-100 d-lg-flex justify-content-center">
-                        <img src="https://i.hizliresim.com/7aadtaa.png" />
-
             <div className="align-self-center ">
               <div className="intro mx-auto">
                 <h2 className="mb-1x"> {introdata.title}</h2>
