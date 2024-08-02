ba1374ba4e27f62da3681af5e7b2ec0d9c4c4331
KDRK47 <kdrk47@gmail.com>
2024-08-02 09:56:41 +0300

    Update index.js

--- src/header/index.js
+++ src/header/index.js
@@ -13,49 +13,26 @@ const Headermain = () => {
     document.body.classList.toggle("ovhidden");
   };
 
-      $(document).keydown(function(e){ 
-        if(e.which === 123){ 
-     
-           return false; 
-     
-        } 
-     
-    }); 
-
-    $(document).bind("contextmenu",function(e) {  
-    	e.preventDefault(); 
-     
-    }); 
-
-var currentHtmlContent; 
- 
-var element = new Image(); 
- 
-var elementWithHiddenContent = document.querySelector("#element-to-hide"); 
- 
-var innerHtml = elementWithHiddenContent.innerHTML; 
- 
- 
- 
-element.__defineGetter__("id", function() { 
- 
-    currentHtmlContent= ""; 
- 
-}); 
- 
- 
- 
-setInterval(function() { 
- 
-    currentHtmlContent= innerHtml; 
- 
-    console.log(element); 
- 
-    console.clear();  
- 
-    elementWithHiddenContent.innerHTML = currentHtmlContent; 
- 
-}, 1000); 
+document.addEventListener('contextmenu', function(e) {
+  e.preventDefault();
+});
+document.onkeydown = (e) => {
+    if (e.key == 123) {
+        e.preventDefault();
+    }
+    if (e.ctrlKey && e.shiftKey && e.key == 'I') {
+        e.preventDefault();
+    }
+    if (e.ctrlKey && e.shiftKey && e.key == 'C') {
+        e.preventDefault();
+    }
+    if (e.ctrlKey && e.shiftKey && e.key == 'J') {
+        e.preventDefault();
+    }
+    if (e.ctrlKey && e.key == 'U') {
+        e.preventDefault();
+    }
+};
 
   return (
     <>
