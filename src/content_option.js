681f8057c81a4293825a95512d72582c2d494439
KDRK47 <kdrk47@gmail.com>
2024-08-02 13:47:55 +0300

    Update content_option.js

--- src/content_option.js
+++ src/content_option.js
@@ -11,7 +11,7 @@ const introdata = {
         second: "IT Manager",
         third: "IT Manager",
     },
-    description: `Sektörde 23 yılı aşkın BT deneyimiyle, sistem ve ağ yönetimi, siber güvenlik, proje yönetimi ve bulut teknolojileri yöneticisiyim.`,
+    description: `Sektörde 23 yılı aşkın BT deneyimiyle, sistem ve ağ yönetimi, siber güvenlik, proje yönetimi ve bulut teknolojileri \nyöneticisiyim.`,
     your_img_url: "https://i.hizliresim.com/79d3xa8.jpeg",
     logoimage_url: "https://i.hizliresim.com/s4oqghk.png",
 };
