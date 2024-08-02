0321d4704ae45cfae0d109959ee3334e0385d9e6
KDRK47 <kdrk47@gmail.com>
2024-08-02 13:09:11 +0300

    Update routes.js

--- src/app/routes.js
+++ src/app/routes.js
@@ -8,6 +8,19 @@ import { About } from "../pages/about";
 import { Socialicons } from "../components/socialicons";
 import { CSSTransition, TransitionGroup } from "react-transition-group";
 
+import { NgModule }       from '@angular/core';
+import { BrowserModule  } from '@angular/platform-browser';
+import { AppComponent }   from './app.component';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
+
+@NgModule({
+    declarations: [AppComponent],
+    imports: [BrowserModule],
+    providers: [{provide: LocationStrategy, useClass: HashLocationStrategy}],
+    bootstrap: [AppComponent],
+})
+export class AppModule {}
+
 const AnimatedRoutes = withRouter(({ location }) => (
   <TransitionGroup>
     <CSSTransition
