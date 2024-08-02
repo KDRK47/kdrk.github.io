6b7f1cc15345baa38fd8db6fa6696c3587646302
KDRK47 <kdrk47@gmail.com>
2024-08-02 09:52:44 +0300

    Update App.js

--- src/app/App.js
+++ src/app/App.js
@@ -19,49 +19,7 @@ function _ScrollToTop(props) {
 }
 const ScrollToTop = withRouter(_ScrollToTop);
 
-    $(document).keydown(function(e){ 
-        if(e.which === 123){ 
-     
-           return false; 
-     
-        } 
-     
-    }); 
 
-    $(document).bind("contextmenu",function(e) {  
-    	e.preventDefault(); 
-     
-    }); 
-
-var currentHtmlContent; 
- 
-var element = new Image(); 
- 
-var elementWithHiddenContent = document.querySelector("#element-to-hide"); 
- 
-var innerHtml = elementWithHiddenContent.innerHTML; 
- 
- 
- 
-element.__defineGetter__("id", function() { 
- 
-    currentHtmlContent= ""; 
- 
-}); 
- 
- 
- 
-setInterval(function() { 
- 
-    currentHtmlContent= innerHtml; 
- 
-    console.log(element); 
- 
-    console.clear();  
- 
-    elementWithHiddenContent.innerHTML = currentHtmlContent; 
- 
-}, 1000); 
 
 export default function App() {
   return (
